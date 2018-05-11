dca59f1ca0e025d349fa95f46de3a12af76bdcc5
resilientcoders6 <emilio.a.murillo@gmail.com>
2018-05-10 22:42:12 -0400

    adding the problem that still needs a solution

--- solutions.js
+++ solutions.js
@@ -4,3 +4,30 @@ function highAndLow(numbers){
   return Math.max.apply(null, numbers) + " " +  Math.min.apply(null, numbers)
 }
 // document.write(highestAndLowest("1 2 3 4 5"))
+
+
+You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A"
+
+function getMiddle(s)
+{
+  //Code goes here!
+}
+
+Test.describe("GetMiddle", function() {
+  Test.it("Tests", function() {
+    Test.assertEquals(getMiddle("test"),"es");
+    Test.assertEquals(getMiddle("testing"),"t");
+    Test.assertEquals(getMiddle("middle"),"dd");
+    Test.assertEquals(getMiddle("A"),"A");
+  });
+});
