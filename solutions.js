170f589921870a8a558b2baade6f575393207e24
resilientcoders6 <emilio.a.murillo@gmail.com>
2018-05-11 16:21:47 -0400

    adding solution 3 of week one

--- solutions.js
+++ solutions.js
@@ -29,3 +29,17 @@ function getMiddle(s)
     return odd;
   });
 });
+
+
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw"
+spinWords( "This is a test") => returns "This is a test"
+spinWords( "This is another test" )=> returns "This is rehtona
+
+function spinWords(words){
+    words = words.split(' ');
+    for(var i=0, item; item=words[i]; i++) {
+        if(item.length >=5) words[i] = item.split('').reverse().join('');
+    }
+    return words.join(' ');
+}
